ac8b2c95c5e92090b7d8fd331dc1eb0731b94e0b
Fer <fernando.pazo.gonzalez@udc.es>
2025-02-18 17:28:02 +0100

    Switches

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ void led_red_init()
 {
   // SIM->SCGC5
   SIM->SCGC5 |= SIM_SCGC5_PORTE(1);
-  // PORTD->PCR[5]
+  // PORTD->PCR[29]
   PORTE->PCR[29] |= PORT_PCR_MUX(1);
   // GPIOD->PDDR
   GPIOE->PDDR |= GPIO_PDDR_PDD(1<<29);
@@ -49,15 +49,48 @@ void led_red_toggle(void)
   GPIOE->PTOR |= GPIO_PTOR_PTTO(1<<29);
 }
 
+void sw_1_init(){
+  SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
+  PORTC->PCR[3] |= PORT_PCR_MUX(1);
+  // Pull enable
+  PORTC->PCR[3] |= PORT_PCR_PE(1);
+  GPIOC->PDDR |= GPIO_PDDR_PDD(1<<3);
+  GPIOC->PSOR |= GPIO_PSOR_PTSO(1<<3);
+
+}
+
+int green_act(){
+  return ((GPIOC->PDIR & (1<<3)) == 0);
+}
+
+void sw_3_init(){
+  SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
+  PORTC->PCR[12] |= PORT_PCR_MUX(1);
+  // Pull enable
+  PORTC->PCR[12] |= PORT_PCR_PE(1);
+  GPIOC->PDDR |= GPIO_PDDR_PDD(1<<12);
+  GPIOC->PSOR |= GPIO_PSOR_PTSO(1<<12);
+}
+
+int red_act(){
+  return ((GPIOC->PDIR & (1<<12)) == 0);
+}
+
 int main(void)
 {
   led_green_init();
   led_red_init();
+  sw_1_init();
+  sw_3_init();
 
   while (1) {
-    led_green_toggle();
-    delay();
-    led_red_toggle();
+    if(green_act()){
+      led_green_toggle();
+      while(green_act()){}
+    }else if(red_act()){
+      led_red_toggle();
+      while(green_act()){}
+    }
   }
 
   return 0;
